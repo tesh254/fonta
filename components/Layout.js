728cfedee0c4bc35363334351e7fcf2bfb73c89b
wachira_Dev <ewachira254@gmail.com>
2022-01-14 16:27:45 +0300

    @fonta/ui[improvements]: remove meta image

--- components/Layout.js
+++ components/Layout.js
@@ -22,18 +22,18 @@ export default function Layout({ children, meta: pageMeta, title }) {
         <meta content={meta.description} name="description" />
         <meta
           property="og:url"
-          content={`https://subscription-starter.vercel.app${router.asPath}`}
+          content={`https://fonta.vercel.app${router.asPath}`}
         />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={title || meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={title || meta.title} />
-        <meta property="og:image" content={meta.cardImage} />
+        {/* <meta property="og:image" content={meta.cardImage} /> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@vercel" />
         <meta name="twitter:title" content={title || meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.cardImage} />
+        {/* <meta name="twitter:image" content={meta.cardImage} /> */}
       </Head>
       <Navbar />
       <main id="skip">
